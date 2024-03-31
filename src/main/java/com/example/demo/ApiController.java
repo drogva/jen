8f75cad70dc22665bc15161b0e674352d8f0dba8
drogva <156391770+drogva@users.noreply.github.com>
2024-03-31 20:42:23 +0900

    Update ApiController.java

--- src/main/java/com/example/demo/ApiController.java
+++ src/main/java/com/example/demo/ApiController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class ApiController {
