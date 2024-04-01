278e901a1b4dc36b053e1cad963adac7adb96442
drogva <156391770+drogva@users.noreply.github.com>
2024-04-01 20:56:06 +0900

    Update ApiController.java

--- src/main/java/com/example/demo/ApiController.java
+++ src/main/java/com/example/demo/ApiController.java
@@ -6,16 +6,32 @@ import org.springframework.web.bind.annotation.*;
 public class ApiController {
 
     @RequestMapping(value = "/api/super-shy", method = RequestMethod.PATCH)
-    public ResponseEntity<String> handleSuperShyPatchRequest() {
+    public ResponseEntity<MyResponse> handleSuperShyPatchRequest() {
         // PATCH 요청을 처리하는 로직
-        String responseMessage = "Super Shy PATCH 요청이 처리되었습니다.";
-        return new ResponseEntity<>(responseMessage, HttpStatus.OK);
+        MyResponse response = new MyResponse("Super Shy PATCH 요청이 처리되었습니다.");
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
     
     @RequestMapping(value = "/api/super-shy", method = RequestMethod.GET)
-    public ResponseEntity<String> handleSuperShyGetRequest() {
+    public ResponseEntity<MyResponse> handleSuperShyGetRequest() {
         // GET 요청을 처리하는 로직
-        String responseMessage = "Super Shy GET 요청이 처리되었습니다.";
-        return new ResponseEntity<>(responseMessage, HttpStatus.OK);
+        MyResponse response = new MyResponse("Super Shy GET 요청이 처리되었습니다.");
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+    
+    static class MyResponse {
+        private String message;
+
+        public MyResponse(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
     }
 }
