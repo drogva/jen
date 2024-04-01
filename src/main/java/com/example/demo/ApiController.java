20ac17e7a7ecfa5efcca340730921e328fad5d0f
drogva <156391770+drogva@users.noreply.github.com>
2024-04-01 19:14:31 +0900

    Update ApiController.java

--- src/main/java/com/example/demo/ApiController.java
+++ src/main/java/com/example/demo/ApiController.java
@@ -1,19 +1,21 @@
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ApiController {
 
-    @RequestMapping(value = "/api/super-shy", method = RequestMethod.PATCH) // PATCH 요청을 처리하는 메서드
-    public String handleSuperShyPatchRequest() {
-        // PATCH 요청을 처리하는 로직을 여기에 구현합니다.
-        return "Super Shy PATCH 요청이 처리되었습니다.";
+    @RequestMapping(value = "/api/super-shy", method = RequestMethod.PATCH)
+    public ResponseEntity<String> handleSuperShyPatchRequest() {
+        // PATCH 요청을 처리하는 로직
+        String responseMessage = "Super Shy PATCH 요청이 처리되었습니다.";
+        return new ResponseEntity<>(responseMessage, HttpStatus.OK);
     }
     
-    @RequestMapping(value = "/api/super-shy", method = RequestMethod.GET) // GET 요청을 처리하는 메서드
-    public String handleSuperShyGetRequest() {
-        // GET 요청을 처리하는 로직을 여기에 구현합니다.
-        return "Super Shy GET 요청이 처리되었습니다.";
+    @RequestMapping(value = "/api/super-shy", method = RequestMethod.GET)
+    public ResponseEntity<String> handleSuperShyGetRequest() {
+        // GET 요청을 처리하는 로직
+        String responseMessage = "Super Shy GET 요청이 처리되었습니다.";
+        return new ResponseEntity<>(responseMessage, HttpStatus.OK);
     }
 }
